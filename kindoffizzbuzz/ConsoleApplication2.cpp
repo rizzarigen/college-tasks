7b58adede0e7a7a88ca51c29f12dfbb4660f975a
rizzarigen <110987202+rizzarigen@users.noreply.github.com>
2025-02-26 00:09:38 +0300

    fizzbuzz domino
    быстренько накодил задачку на плюсах

---
+++ kindoffizzbuzz/ConsoleApplication2.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int l;
+int s;
+
+int main()
+{
+	int k;
+	int n;
+
+
+	cin >> k >> n;
+
+	if (k <= 1000 && k >= 1) {
+		if (n <= pow(10, 6) && n >= 1) {
+
+			int *nums = new int[n];
+
+			for (int i = 0; i < n; i++) {
+
+				cin >> nums[i];
+
+				if (nums[i] % 3 == 0) {
+					s += 1;
+				}
+				if (nums[i] % 5 == 0) {
+					l += 1;
+				}
+
+			}
+
+			if (s == l) {
+				cout << "Draw" << endl;
+			}
+			else if (s > l) {
+				cout << "Sasha" << endl;
+			}
+			else {
+				cout << "Lena" << endl;
+			}
+
+		}
+	}
+}
